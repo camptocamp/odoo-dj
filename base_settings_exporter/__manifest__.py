2371c13fe8717e753495f8109d4cba36c8793fad
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-06-07 17:27:31 +0200

    Add module base_settings_exporter
    This module adds a button "Export config" on each settings wizard.
    
    This button will open a popup containing the config to use in songs
    scripts to replay those settings.

---
+++ base_settings_exporter/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{'name': 'Base settings exporter',
+ 'summary': "Add export button to generate songs config",
+ 'version': '10.0.1.0.0',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'license': 'AGPL-3',
+ 'category': 'Hidden',
+ 'depends': ['base_setup'],
+ 'website': 'www.camptocamp.com',
+ 'data': ['wizard/settings_exporter_view.xml'],
+ 'images': [
+    'images/button.png',
+    'images/wizard.png',
+ ],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ }
+
