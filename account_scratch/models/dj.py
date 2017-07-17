a4219818aaf294d51d558c4f038955a8a2ad5fb0
Simone Orsi <simahawk@gmail.com>
2017-07-17 16:53:37 +0200

    draft improvements

--- account_scratch/models/dj.py
+++ account_scratch/models/dj.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from jinja2 import Environment
+
 import json
 
 from odoo import api, fields, models
-from odoo.addons.base_scratch.jinja.disc_loader import DiscLoader
-
-
-class AccountingDJSample(models.Model):
-    """ Specific filter for ir.sequences """
-    _inherit = 'dj.sample'
 
 
 class AccountingDJ(models.Model):
     _inherit = 'dj'
 
-    name = fields.Selection(selection_add=[('account', 'Accounting')])
+    name = fields.Selection(selection_add=[('accounting', 'Accounting')])
     active_currencies = fields.Boolean(default=True)
 
     @api.multi
@@ -28,7 +22,7 @@ class AccountingDJ(models.Model):
         return cc.mapped('name')
 
     @api.multi
-    def get_currency_rate_live_beats(self):
+    def get_currency_rate_live_tvars(self):
         # get active currencies
         cp = self.env['res.company'].search([])[0]
         return {
@@ -57,33 +51,9 @@ class AccountingDJ(models.Model):
         return json.dumps(res, sort_keys=True, indent=4)
 
     @api.multi
-    def get_beats(self):
-        beats = super(AccountingDJ, self).get_beats()
-        beats['currencies'] = self.get_active_currencies()
-        beats.update(self.get_currency_rate_live_beats())
-        beats['sequence_config'] = self.get_sequence_config()
-        return beats
-
-    @api.multi
-    def play_accounting_disc(self):
-        disc = 'accounting.disc'
-        # load Jinja template
-        jnj_env = Environment(loader=DiscLoader('account_scratch'))
-        jnj_template = jnj_env.get_template(disc)
-        kwargs = self.get_beats()
-        return jnj_template.render(**kwargs)
-
-    @api.multi
-    def get_all_tracks(self):
-        files = super(AccountingDJ, self).get_all_tracks()
-
-        if self.name == 'account':
-
-            disc_content = self.play_accounting_disc()
-
-            files.append((
-                'songs/install/accounting.py',
-                disc_content
-            ))
-
-        return files
+    def get_template_vars(self):
+        tvars = super(AccountingDJ, self).get_template_vars()
+        tvars['currencies'] = self.get_active_currencies()
+        tvars.update(self.get_currency_rate_live_tvars())
+        tvars['sequence_config'] = self.get_sequence_config()
+        return tvars
--- base_scratch/models/dj.py
+++ base_scratch/models/dj.py
@@ -8,36 +8,122 @@ import csv
 import logging
 import zipfile
 import io
+import jinja2
+import os
 
 from odoo import models, fields, api
 from odoo.tools.safe_eval import safe_eval
-
+from odoo.modules.module import get_module_resource
 _logger = logging.getLogger(__name__)
+
+
+class DJ(models.Model):
+    """Use discs to create songs from scratch and save it in compact format"""
+
+    _name = 'dj'
+
+    name = fields.Selection([])
+    sample_ids = fields.One2many('dj.sample', 'dj_id')
+    disc_template = fields.Char(default='base_scratch:discs/default.disc')
+    disc_path = fields.Char(default='songs/install/{name}')
+    compact_disc = fields.Binary(
+        help="Resulting Zip file with all songs and related files",
+        attachment=True)
+    album_title = fields.Char(default='out.zip')
+
+    @api.model
+    def check_company_codename(self):
+        """ Check company short codenames have been setup in multi company
+        We need those to create unique codenames
+        """
+        Company = self.env['res.company']
+        company_num = Company.search_count([])
+        if company_num == 1:
+            return
+        companies = Company.search([('aka', '=', False)])
+        if companies:
+            raise  # TODO missing aka
+
+    @api.multi
+    def get_all_tracks(self):
+        """ Return all csv generated from dj.samples """
+        self.ensure_one()
+        files = []
+        for sample in self.sample_ids:
+            files.extend(sample.burn_track())
+        files.append(self.burn_disc())
+        return files
+
+    @api.multi
+    def get_template_vars(self):
+        """ Return list of variable for Jinja
+
+        Get name model and path of samples.
+
+        To be inherited
+        """
+        self.ensure_one()
+        return {'samples': self.sample_ids}
+
+    @api.multi
+    def get_disc_template(self):
+        self.ensure_one()
+        # load Jinja template
+        mod, filepath = self.disc_template.split(':')
+        filepath = get_module_resource(mod, filepath)
+        path, filename = os.path.split(filepath)
+        return jinja2.Environment(
+            loader=jinja2.FileSystemLoader(path)
+        ).get_template(path)
+
+    @api.multi
+    def burn_disc(self):
+        self.ensure_one()
+        template = self.get_disc_template()
+        path = self.disc_path.format(name=self.name)
+        return path, template.render(**self.get_template_vars())
+
+    @api.multi
+    def burn(self):
+        """ Return a zip file containing all required files """
+        self.check_company_codename()
+        for rec in self:
+            files = rec.get_all_tracks()
+            in_mem_zip = io.BytesIO()
+            with zipfile.ZipFile(in_mem_zip, "w", zipfile.ZIP_DEFLATED) as zf:
+                for filepath, data in files:
+                    zf.writestr(filepath, data)
+            in_mem_zip.seek(0)
+            zip_file = base64.encodestring(in_mem_zip.read())
+            rec.compact_disc = zip_file
 
 
 class Sample(models.Model):
     _name = 'dj.sample'
 
-    name = fields.Char(compute='_compute_sample_name')
     dj_id = fields.Many2one('dj', required=True)
     model_id = fields.Many2one('ir.model', required=True)
+    name = fields.Char(compute='_compute_sample_name')
     csv_path = fields.Char()
     domain = fields.Char(default="[]")
     field_list = fields.Char(help="List of field to export separated by ','")
     xmlid_fields = fields.Char(help="List of field to use to generate unique"
                                     " xmlid separated by ','")
 
-    @api.one
+    @api.multi
     @api.depends('model_id.model')
     def _compute_sample_name(self):
-        self.name = (self.model_id.model or '').replace('.', '_')
+        for item in self:
+            item.name = (item.model_id.model or '').replace('.', '_')
 
     @api.model
     def eval_domain(self):
+        # TODO: use domain widget
+        # https://github.com/OCA/web/pull/672
         return safe_eval(self.domain) or []
 
     @api.model
-    def from_data(self, fields, rows):
+    def csv_from_data(self, fields, rows):
         fp = StringIO()
         writer = csv.writer(fp, quoting=csv.QUOTE_ALL)
 
@@ -67,79 +153,21 @@ class Sample(models.Model):
         fp.close()
         return data
 
-    @api.multi
-    def create_csv(self):
+    def burn_csv(self, items):
         # TODO create xmlid starting with __setup__
         # XXX detect auto generated xmlids
-
-        items = self.env[self.model_id.model].search(self.eval_domain())
         field_names = ['id']
         field_names.extend([f.strip() for f in self.field_list.split(',')])
         if 'company_id' in self.model_id.field_id.mapped('name'):
             field_names.append('company_id')
         export_data = items.export_data(field_names).get('datas', [])
-
-        return self.from_data(field_names, export_data)
-
-
-class DJ(models.Model):
-
-    """Use discs to create songs from scratch and save it in compact format"""
-
-    _name = 'dj'
-
-    name = fields.Selection([])
-    playlist = fields.One2many('dj.sample', 'dj_id')
-    compact_disc = fields.Binary(
-        help="Resulting Zip file with all songs and related files",
-        attachment=True)
-    album_title = fields.Char(default='out.zip')
-
-    @api.model
-    def check_company_codename(self):
-        """ Check company short codenames have been setup in multi company
-        We need those to create unique codenames
-        """
-        Company = self.env['res.company']
-        company_num = Company.search_count([])
-        if company_num == 1:
-            return
-        companies = Company.search([('aka', '=', False)])
-        if companies:
-            raise  # TODO missing aka
+        return self.csv_path, self.csv_from_data(field_names, export_data)
 
     @api.multi
-    def get_all_tracks(self):
-        """ Return all csv generated from dj.samples """
+    def burn_track(self):
         self.ensure_one()
-        files = []
-        for sample in self.playlist:
-            files.append((
-                sample.csv_path,
-                sample.create_csv()
-            ))
-        return files
-
-    @api.multi
-    def get_beats(self):
-        """ Return list of variable for Jinja
-
-        Get name model and path of playlist
-
-        To be inherited
-        """
-        return {'samples': self.playlist}
-
-    @api.multi
-    def play(self):
-        """ Return a zip file containing all required files """
-        self.check_company_codename()
-        for rec in self:
-            files = rec.get_all_tracks()
-            in_mem_zip = io.BytesIO()
-            with zipfile.ZipFile(in_mem_zip, "w", zipfile.ZIP_DEFLATED) as zf:
-                for filepath, data in files:
-                    zf.writestr(filepath, data)
-            in_mem_zip.seek(0)
-            zip_file = base64.encodestring(in_mem_zip.read())
-            rec.compact_disc = zip_file
+        items = self.env[self.model_id.model].search(self.eval_domain())
+        csv_path, csv_data = self.burn_csv(items)
+        return [
+            (csv_path, csv_data),
+        ]
