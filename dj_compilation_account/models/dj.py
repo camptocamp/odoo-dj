fc202450340722d4f05b407472b6adb9b9c7426a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-08-11 13:21:27 +0200

    Remove relicates for sequences

--- dj_compilation_account/models/dj.py
+++ dj_compilation_account/models/dj.py
@@ -2,8 +2,6 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-import json
-
 from odoo import api, models
 
 
@@ -18,30 +16,3 @@ class AccountingDJ(models.Model):
             'currency_interval_unit': cp.currency_interval_unit,
             'currency_provider': cp.currency_provider,
         }
-
-    @api.multi
-    def get_sequence_config(self):
-        Journal = self.env['account.journal']
-        ModelData = self.env['ir.model.data']
-        res = {}
-        for cp in self.env['res.company'].search([]):
-            xmlid = ModelData.search([
-                ('model', '=', 'res.company'),
-                ('res_id', '=', cp.id)]).complete_name
-            journals = Journal.search([
-                ('code', '=', ('INV', 'BILL')),
-                ('company_id', '=', cp.id)]
-            )
-
-            res[xmlid] = {j.code: [
-                j.sequence_id.prefix, j.sequence_id.padding,
-                j.refund_sequence_id.prefix, j.refund_sequence_id.padding]
-                for j in journals}
-        return json.dumps(res, sort_keys=True, indent=4)
-
-    @api.multi
-    def get_template_vars(self):
-        tvars = super(AccountingDJ, self).get_template_vars()
-        tvars.update(self.get_currency_rate_live_tvars())
-        tvars['sequence_config'] = self.get_sequence_config()
-        return tvars
