0c3e6b1c6da98f820a9df0d3d5ae15893ac9f0ce
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-08-16 18:06:23 +0200

    New module to inclue models from enterprise

---
+++ dj_compilation_account_enterprise/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'DJ set - Accounting compilation - ',
+    'summary': "Extension for Enterprise models only",
+    'version': '10.0.1.0',
+    'author': 'Camptocamp',
+    'maintainer': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'songs',
+    'depends': ['dj_compilation_account', 'account_reports'],
+    'website': 'www.camptocamp.com',
+    'data': ['data/dj.xml'],
+    'test': [],
+    'external_dependencies': {
+    },
+    'installable': True,
+    'auto_install': True,
+}
