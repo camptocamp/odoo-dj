1d4fc04fc00a5e9c6061ee4b79eb66dba09a8fa9
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-08-22 17:53:49 +0200

    Enterprise in manifest module name

--- dj_compilation_account_enterprise/__manifest__.py
+++ dj_compilation_account_enterprise/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'DJ set - Accounting compilation - ',
+    'name': 'DJ set - Accounting compilation - Enterprise',
     'summary': "Extension for Enterprise models only",
     'version': '10.0.1.0.0',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
