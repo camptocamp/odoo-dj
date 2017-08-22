9179507b4049b711f84c0ff7cd54e2da2c614223
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-08-22 14:38:54 +0200

    Create module for account_payment_mode

--- dj_compilation_account_payment_mode/__manifest__.py
+++ dj_compilation_account_payment_mode/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'DJ set - Accounting compilation',
+    'name': 'DJ set - Accounting compilation for payment modes',
     'summary': "Create accounting songs from scratch",
     'version': '10.0.1.0.0',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
     'maintainer': 'Camptocamp',
     'license': 'AGPL-3',
     'category': 'songs',
-    'depends': ['base_dj', 'account'],
+    'depends': ['base_dj', 'account', 'account_payment_mode'],
     'website': 'www.camptocamp.com',
     'data': ['data/dj.xml'],
     'test': [],
     'external_dependencies': {
     },
     'installable': True,
-    'auto_install': False,
+    'auto_install': True,
 }
