a34b8083a9afde5bae28cf18229ea62aff5b803c
Simone Orsi <simahawk@gmail.com>
2017-10-09 13:10:19 +0200

    [imp] dj.equalizer: hold global config not just xmlid (closes #42)

--- base_dj/models/dj_equalizer.py
+++ base_dj/models/dj_equalizer.py
@@ -3,18 +3,31 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
 
 
-class EqualizerXMLID(models.Model):
-    """Hold models' configuration to generate xmlid."""
+class DJEqualizer(models.Model):
+    """Hold models' global configuration."""
 
-    _name = 'dj.equalizer.xmlid'
+    _name = 'dj.equalizer'
 
-    model = fields.Char()
-    xmlid_fields = fields.Char()
+    model = fields.Char(default='')
+    xmlid_fields = fields.Char(default='')
+    model_context = fields.Char(default='{}')
 
     @api.multi
     def get_xmlid_fields(self):
         self.ensure_one()
         return [x.strip() for x in self.xmlid_fields.split(',')
-                if x.strip()]
+                if x.strip()] if self.xmlid_fields else []
+
+    @api.multi
+    def get_model_context(self):
+        self.ensure_one()
+        return safe_eval(self.model_context) if self.model_context else {}
+
+    def get_conf(self):
+        return {
+            'xmlid_fields': self.get_xmlid_fields(),
+            'model_context': self.get_model_context(),
+        }
