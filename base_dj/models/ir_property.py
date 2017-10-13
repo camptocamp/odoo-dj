f8b4793c54be65d0a2cbc0f60c5c782155dc6f8f
Simone Orsi <simahawk@gmail.com>
2017-10-13 15:22:42 +0200

    [fix] ir.property: convert also `res_id` to xmlid (fixes #82)

--- base_dj/models/ir_property.py
+++ base_dj/models/ir_property.py
@@ -9,26 +9,28 @@ from ..utils import property_to_xmlid, xmlid_to_property
 class Property(models.Model):
 
     _inherit = 'ir.property'
+    # these fields come in the form `model,ID`
+    _property_like_fields_to_update = ('value_reference', 'res_id', )
 
     @api.multi
     def _update_values(self, values):
-        """Inverse xmlid value to reference value."""
-        if (self.env.context.get('xmlid_value_reference', False) and
-                values.get('value_reference')):
-            values['value_reference'] = \
-                xmlid_to_property(values['value_reference'])
+        """Inverse xmlid values to property values."""
+        if self.env.context.get('xmlid_value_reference'):
+            for fname in self._property_like_fields_to_update:
+                if values.get(fname):
+                    values[fname] = xmlid_to_property(self.env, values[fname])
         return super(Property, self)._update_values(values)
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
-        """Get `value_reference` as xmlid."""
+        """Convert property values to xmlid."""
         res = super(Property, self).read(fields=fields, load=load)
         if not self.env.context.get('xmlid_value_reference'):
             return res
         # wipe cache otherwise we gonna get the std value in any case
-        self.invalidate_cache(['value_reference'])
+        self.invalidate_cache(self._property_like_fields_to_update)
         for rec in res:
-            if rec.get('value_reference'):
-                rec['value_reference'] = property_to_xmlid(
-                    self.env, rec['value_reference'])
+            for fname in self._property_like_fields_to_update:
+                if rec.get(fname):
+                    rec[fname] = property_to_xmlid(self.env, rec[fname])
         return res
