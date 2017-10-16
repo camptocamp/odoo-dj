ab7fd3b97f98fb22969e3dcf957b10c94e06dde1
Simone Orsi <simahawk@gmail.com>
2017-10-16 17:18:10 +0200

    [add] dj_core WIP (closes #48)

---
+++ dj_core/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'DJ Core compilations and configurations',
+    'summary': "Basic configurations and records for all compilations",
+    'version': '10.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'songs',
+    'depends': ['base_dj', ],
+    'website': 'www.camptocamp.com',
+    'data': ['data/dj.xml'],
+    'installable': True,
+    'auto_install': False,
+}
