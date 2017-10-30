3892650f23059c6d5891f6d3f8ad3e70727c2427
Simone Orsi <simahawk@gmail.com>
2017-10-30 17:24:34 +0100

    [fix] setuptool include all files

--- setup/base_dj/setup.py
+++ setup/base_dj/setup.py
@@ -3,4 +3,6 @@ import setuptools
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon={'odoo_version_override': '10.0'},
+    include_package_data=True,
+    package_data={'': ['*', ]}
 )
