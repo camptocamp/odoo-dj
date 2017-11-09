2335ed3aad209385c10866b041488029b872750e
Simone Orsi <simahawk@gmail.com>
2017-11-09 17:47:30 +0100

    Add base tests \o/ and autopep8 (fixes #98, addresses #51)

--- setup/base_dj/setup.py
+++ setup/base_dj/setup.py
@@ -2,7 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={'odoo_version_override': '10.0'},
-    include_package_data=True,
-    package_data={'': ['*', ]}
+    odoo_addon=True,
 )
