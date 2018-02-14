239d4b851453122b0cf1f86e67f4b6887048da32
Simone Orsi <simahawk@gmail.com>
2018-02-14 16:34:34 +0100

    Fix csv fields: unique and ordered

--- base_dj/models/dj/dj_song.py
+++ base_dj/models/dj/dj_song.py
@@ -464,11 +464,16 @@ class Song(models.Model):
         if ('company_id' in self.song_model and
                 'company_id/id' not in field_names):
             field_names.append('company_id/id')
+        # make fields unique and keep always the same ordering
+        field_names = sorted((set(field_names)))
+        # make sure `id` is always 1st
+        field_names.remove('id')
+        field_names.insert(0, 'id')
         if 'name' in field_names:
             # make sure is always after `id` to ease csv review
             field_names.remove('name')
             field_names.insert(1, 'name')
-        return [to_str(x) for x in set(field_names)]
+        return [to_str(x) for x in field_names]
 
     def get_csv_field_names_exclude(self):
         """Return fields that must be imported in 2 steps.
