f656463e5646c32d7f2da760f738f686bf2d17b3
Simone Orsi <simahawk@gmail.com>
2018-02-14 16:41:06 +0100

    Fix tests

--- base_dj/tests/__init__.py
+++ base_dj/tests/__init__.py
@@ -1,6 +1,7 @@
 from . import test_compilation
 from . import test_song
-from . import test_settings_song
+# deprecated
+# from . import test_settings_song
 from . import test_xmlid
 from . import test_defaults
 from . import test_song_addons
--- base_dj/tests/fixtures/fixture_comp1.py
+++ base_dj/tests/fixtures/fixture_comp1.py
@@ -12,33 +12,32 @@ from ...common import load_csv
 @anthem.log
 def load_res_company(ctx):
     """ Import res.company from csv """
+    path = 'data/install/generated/dj_test/comp1/res.company.csv'
     model = ctx.env['res.company'].with_context(tracking_disable=True)
     header_exclude = ['parent_id/id']
-    load_csv(ctx, 'data/install/generated/dj_test/comp1/res.company.csv',
-             model, header_exclude=header_exclude)
+    load_csv(ctx, path, model, header_exclude=header_exclude)
     if header_exclude:
-        load_csv(ctx, 'data/install/generated/dj_test/comp1/res.company.csv',
-                 model, header=['id', ] + header_exclude)
+        load_csv(ctx, path, model)
 
 
 @anthem.log
 def load_res_users(ctx):
     """ Import res.users from csv """
+    path = 'data/install/generated/dj_test/comp1/res.users.csv'
     model = ctx.env['res.users'].with_context(
         no_reset_password=True, tracking_disable=True)
-    load_csv(ctx, 'data/install/generated/dj_test/comp1/res.users.csv', model)
+    load_csv(ctx, path, model)
 
 
 @anthem.log
 def load_res_partner(ctx):
     """ Import res.partner from csv """
+    path = 'data/install/generated/dj_test/comp1/res.partner.csv'
     model = ctx.env['res.partner'].with_context(tracking_disable=True)
     header_exclude = ['commercial_partner_id/id', 'parent_id/id']
-    load_csv(ctx, 'data/install/generated/dj_test/comp1/res.partner.csv',
-             model, header_exclude=header_exclude)
+    load_csv(ctx, path, model, header_exclude=header_exclude)
     if header_exclude:
-        load_csv(ctx, 'data/install/generated/dj_test/comp1/res.partner.csv',
-                 model, header=['id', ] + header_exclude)
+        load_csv(ctx, path, model)
 
 
 @anthem.log
