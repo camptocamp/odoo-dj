f0d53f33f165f640d5c54ef0b8864ce7d41f4286
Simone Orsi <simahawk@gmail.com>
2018-03-21 12:56:51 +0100

    Add `dj_survey`

---
+++ dj_survey/README.rst
@@ -0,0 +1,8 @@
+DJ Survey
+=======
+
+Default core stuff survey configurations.
+
+Includes:
+
+* survey config params
---
+++ dj_survey/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'DJ Survey compilations and configurations',
+    'summary': "Basic configurations and records for survey compilations",
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'website': 'https://github.com/camptocamp/odoo-dj',
+    'license': 'AGPL-3',
+    'category': 'songs',
+    'depends': ['dj_core', 'survey'],
+    'data': [
+        'data/equalizer.xml',
+        'data/dj.xml',
+    ],
+}
