74c1b4f60da87f610abb2d7aba83a6420629fe5c
Timon Tschanz <timon.tschanz@gmail.com>
2018-09-28 13:09:32 +0200

    Add dj_crm module (#116)
    * Add dj_crm module
    
    * add crm_team song
    
    * improve mail_alias domain
    
    * requested changes

---
+++ dj_crm/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'DJ CRM compilations and configurations',
+    'summary': "Basic configurations and records for crm compilations",
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'website': 'https://github.com/camptocamp/odoo-dj',
+    'license': 'AGPL-3',
+    'category': 'songs',
+    'depends': ['dj_core', 'crm'],
+    'data': [
+        'data/equalizer.xml',
+        'data/dj.xml',
+    ],
+}
